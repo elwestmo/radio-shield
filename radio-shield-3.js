9786529a1b02a754cd3790aed670e5d49948d2ab
Eric <elwestmo@gmail.com>
2014-05-06 08:32:35 -0400

    Update radio-shield-3.js

--- radio-shield-3.js
+++ radio-shield-3.js
@@ -1,13 +1,14 @@
-// radio shield 3
-// yet another fork of radio-shield.js
+<script>// radio shield 3a
+// yet another fork (branch?) of radio-shield.js // adding a more noticible highlight on click
 $(document).ready(function(){
 	var radioShield = {};
-	$('.ed-noscript').css({'display': 'none'})
+	
 //	radio shield
-	$('input[id^="eventChoice"]').hide();
-	$('label[for^="eventChoice"]').addClass('h3');
+	$('input[id^="PurpleOrGold"]').hide();
+	$('label[for^="PurpleOrGold"]').addClass('h3');
+	$('.formRequired').eq(0).hide(); // -- this may need to be adjusted -- this is hiding the first required things requirement label (*), i'm assuming the shield radios are the first required on the page,
 
-	$('label[for^="eventChoice"]').css({
+	$('label[for^="PurpleOrGold"]').css({
 		'background': 'url(\'https://www.albion.edu/images/sites/websupport/shield/radio-shield-vert-20.png\') 0 0 no-repeat transparent',
 		 'display' : 'inline-block',
 		  'height': '26px',
@@ -15,14 +16,14 @@ $(document).ready(function(){
 		});
 
 	
-	$('label[for^="eventChoice"]').on('mouseenter', function(){
+	$('label[for^="PurpleOrGold"]').on('mouseenter', function(){
 		if ( !$(this).hasClass('activeLabel') ){
 			$(this).css({
 				'background-position': '0 -26px'
 			});
 		}
 	});
-	$('label[for^="eventChoice"]').on('mouseout', function(){
+	$('label[for^="PurpleOrGold"]').on('mouseout', function(){
 		if ( !$(this).hasClass('activeLabel') ){
 			$(this).css({
 				'background-position': '0 0px'
@@ -30,9 +31,9 @@ $(document).ready(function(){
 		}
 	});
 
-	$('label[for^="eventChoice"]').on('mousedown', function(){
+	$('label[for^="PurpleOrGold"]').on('mousedown', function(){
 		//reset the all background images
-		$('label[for^="eventChoice"]').css({
+		$('label[for^="PurpleOrGold"]').css({
 			'background-position': '0 0px'
 		}).removeClass('activeLabel');
 		//move the current label's background image
@@ -42,19 +43,32 @@ $(document).ready(function(){
 		
 		radioShield.clickedLink = $(this).attr('for');
 		$(this).prev().attr('checked', 'checked');
-
-		
-
-
-
-
-
-		// append matching p to clickedLink
-		$('[class^="details-insert-"]').remove();
-		radioShield.currentThing = '<div class="details-insert-' + radioShield.clickedLink + '" style="display:none;">' + $("." + radioShield.clickedLink + "-event-details").html() + "</div>";
-		$(this).after( radioShield.currentThing );
-		$('[class^="details-insert-"]').fadeIn().css({'margin-left': '30px'});
-		// /append matching p to clickedLink
-
 	});//on click
 //	/radio shield
+
+// Purple and Gold Specific
+$('label[for=\"PurpleOrGold1\"]').css({'color': '#D69D00'});
+/*
+// default - #685C53
+$('label[for^=\"PurpleOrGold\"]').css({'color': '#685C53'});
+
+// purple - #592041
+$('label[for=\"PurpleOrGold0\"]').on('mouseenter', function(){
+	$(this).css({'color': '#592041'});
+});
+$('label[for=\"PurpleOrGold0\"]').on('mouseout', function(){
+	$(this).css({'color': '#685C53'});
+});
+// gold - #D69D00
+$('label[for=\"PurpleOrGold1\"]').on('mouseenter', function(){
+	$(this).css({'color': '#D69D00'});
+});
+$('label[for=\"PurpleOrGold1\"]').on('mouseout', function(){
+	$(this).css({'color': '#685C53'});
+});
+*/
+// /Purple and Gold Specific
+
+
+}); //	/document ready
+</script>
